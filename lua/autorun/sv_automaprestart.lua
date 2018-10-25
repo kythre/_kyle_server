c0228e8b9f135db928d49b52a151c9a1eae7ad01
Kyle <thisisnotkylesemail@gmail.com>
2018-10-24 23:02:49 -0500

    Update lua/autorun/sv_automaprestart.lua

--- lua/autorun/sv_automaprestart.lua
+++ lua/autorun/sv_automaprestart.lua
@@ -2,6 +2,7 @@ hook.Add( "PlayerDisconnected", "autorestart", function()
     timer.Simple(300, function()
         if #player.GetAll() == 0 then
 			if	DiscordRelay then
+				local date = os.date( "*t" )
 				DiscordRelay.SendToDiscordRaw(nil, nil,  string.format( "[%02i:%02i:%02i] ", date.hour, date.min, date.sec ) .. "server empty for 5 minutes, map restarting" )
 			end
 			print("server empty for 5 minutes, map restarting")
